083fb9f610b64bd7307b3b480bec7a7edba4a6ae
Pavlo Vovchanovsky <pasha.vovchanovsky@yandex.ru>
2017-03-10 09:26:32 -0200

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Pupil
+{
+protected:
+	string school;
+	string name;
+	int age;
+	void SetSchool();
+	void OutData();
+	void OutAge();
+	void ChangeName();
+	void ChangeAge();
+public:
+	Pupil();
+	Pupil(string, int, string);
+	virtual void Interface();
+};
+
+class Journal : public Pupil
+{
+public:
+	void Interface();
+	Journal();
+	~Journal();
+private:
+	int nm;
+	int *mark = new int[nm];
+	void WatchMarks();
+};
